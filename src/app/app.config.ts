881ee8e24a219b459b3edabac6257a9486c84025
Fernanda <nanda.kipper@gmail.com>
2024-10-23 20:04:43 -0300

    feat: first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { registerLicense } from '@syncfusion/ej2-base';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1JpR2FGfV5ycEVAalxTTnRfUiweQnxTdEFiWX1dcXNQRWJZWEFwXQ==');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideHttpClient(withFetch())
+  ]
 };
